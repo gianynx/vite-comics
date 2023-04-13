42b5520b4409fc004f5aa41c6255436926bc9c25
Gianmarco Incocciati <minusgian@gmail.com>
2023-04-13 16:52:22 +0200

    add loop for inside HeaderComponent.vue

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,17 +1,27 @@
 <template>
     <header>
         <div class="container pt-5">
-            <img src="../assets/img/dc-logo.png" alt="dc-logo">
+            <div id="img_container">
+                <img src="../assets/img/dc-logo.png" alt="dc-logo">
+            </div>
+            <div id="navbar_container">
+                <ul>
+                    <li v-for="(link, index) in navbar" :key="index">
+                        <a href="link.url" :class="{ 'active': link.current }">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </header>
 </template>
 
 <script>
+import { links } from '../assets/data/data.js'
 export default {
     name: 'HeaderComponent',
     data() {
         return {
-
+            navbar: links
         }
     }
 }
